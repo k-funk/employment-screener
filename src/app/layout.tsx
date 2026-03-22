ef0fe7db8d6077fc05a9fcf20a20f4b53a236cac
Kevin Funk <kevincfunk@gmail.com>
2026-03-22 15:49:11 +0100

    fix eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       className={`${inter.variable} ${manrope.variable}${isDark ? ' dark' : ''}`}
     >
       <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
