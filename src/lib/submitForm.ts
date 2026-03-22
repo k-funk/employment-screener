bf89fdae23ad353bfaf6f183620020034207af89
Kevin Funk <kevincfunk@gmail.com>
2026-03-22 21:15:43 +0100

    update google app url

--- src/lib/submitForm.ts
+++ src/lib/submitForm.ts
@@ -2,7 +2,7 @@ import { RecruiterFormData, toSubmissionData } from '@/types/recruiter'
 
 // Note: Any time we 'deploy' a new version of this Google Apps Script, we need to update this URL to point to the new
 // deployment
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzpNZ_BJKEEW9IFKnTkQu_6zOYUDMWNCB2nd9X9HvvNCOnQeM2R9PkefsXa2n3vfCa5QA/exec'
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw7xbJdouHldSX21SSIEjev24qcljVou5EkphyFgwRh2ACeFhFMiCqAGFh3QdSAR0k2wA/exec'
 
 export async function submitForm(data: RecruiterFormData): Promise<void> {
   const response = await fetch(APPS_SCRIPT_URL, {
